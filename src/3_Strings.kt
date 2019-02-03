077646b2646864e2c378a851a0bd5682f14b5b9d
kalug89 <kaluzny_david@hotmail.com>
2019-02-03 16:20:13 +0100

    End string chapter

---
+++ src/3_Strings.kt
@@ -0,0 +1,67 @@
+fun main(args: Array<String>) {
+
+    //1 Zadanie
+//    val str1 = "Hello, World!"
+//    var str2 = "Hello," + "World"
+
+//    println(str1 == str2)
+
+
+    //2 zadanie
+//    val str1 = """
+//        |Hello, World!
+//        """.trimMargin()
+//
+//    val str2 = """
+//        #Hello, World!
+//        """.trimMargin("#")
+//    val str3 = str1
+//    println(str1 == str2)
+//    println(str1 === str2)
+//    println(str1 === str3)
+//
+//    println(str1)
+//    println(str2)
+//    println(str3)
+
+//    val s = "Hello, world!\n"
+//    println(s)
+//
+//        val text = """
+//    |Tell me and I forget.
+//    |Teach me and I remember.
+//    |Involve me and I learn.
+//    |(Benjamin Franklin)
+//    """.trimMargin()
+//
+//    println(text)
+    //3 zadanie
+
+//    val str = "Hello world!"
+//    println(str[1])
+//
+//    for (c in str)
+//        println(c)
+
+    //4 zadanie
+
+//    val i = 10
+//    val s = "i = $i"
+//
+//    println(s)
+
+//    val s = "abc"
+//    val str = "$s.length is ${s.length}"
+//
+//    println(str)
+
+//    val str = "\$foo"
+//    println(str)
+
+    val price = """
+        ${'$'}9.99
+        """
+
+    println(price)
+}
+
