3856dbcc479b86e1055829c5b2cf0da5f8838366
kalug89 <kaluzny_david@hotmail.com>
2019-02-03 17:21:35 +0100

    End chapter of collection

---
+++ src/Collections.kt
@@ -0,0 +1,13 @@
+fun main(args: Array<String>) {
+    //1
+//    val list = listOf("Item 1","Item 2", "Item 3")
+//    println(list)
+
+    //2
+//    val map = mapOf(Pair(1, "Item 1"), Pair(2, "Item 2"), Pair(3, "Item 3"))
+//    println(map)
+
+    //3
+    val set = setOf(1 ,2 ,3)
+    println(set)
+}
