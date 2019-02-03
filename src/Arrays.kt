9b3ac0428fbee5059acae5fd9454ab835e48acbe
kalug89 <kaluzny_david@hotmail.com>
2019-02-03 17:04:16 +0100

    End exercise in Array chapter

---
+++ src/Arrays.kt
@@ -0,0 +1,45 @@
+import java.util.*
+import javax.print.attribute.IntegerSyntax
+
+fun main(args: Array<String>) {
+
+//    val empty = emptyArray<String>()
+//
+//    var strings = Array<String>(size = 5, init = { index -> "Item #$index" })
+//    print(Arrays.toString(a)) // prints "[Item #0, Item #1, Item #2, Item #3, Item #4]"
+//    print(a.size) // prints 5
+//
+//strings.set(2, "ChangeItem")
+//
+//
+
+    //3 Zadanie
+
+//    val a = arrayOf(1,2,3)
+
+    //4 Zadanie
+
+//    val a = Array(3) { i -> i *2}
+
+    //5
+
+//    val b = arrayOfNulls<Int>(3)
+
+    //6
+//    val doubles = doubleArrayOf(1.5,3.0)
+//    print(doubles.average())
+
+//    val ints = intArrayOf(1, 9, 2, 6, 3, 5)
+//    println(ints.max())
+
+    //7
+//val str = Array(5, {i -> (i * i).toString()})
+//    for (a in str){
+//        println(a)
+//    }
+    
+}
+
+
+
+
