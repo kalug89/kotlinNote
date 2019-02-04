3299be7a03be77fc10d0c3c4715b1a686f2a034c
kalug89 <kaluzny_david@hotmail.com>
2019-02-04 21:55:15 +0100

    End Vararg and start Conditional Statements

--- src/8_Vararg Parameters.kt
+++ src/8_Vararg Parameters.kt
@@ -1,3 +1,12 @@
 fun main(args: Array<String>) {
-    
+
+
+    fun myNumber(vararg numbers: Int) {
+        for (number in numbers) {
+            println(number)
+        }
+    }
+
+    var newNumber = intArrayOf(1, 2, 3, 4, 5)
+    myNumber(1, 2, *newNumber, 5, 6, 7)
 }
