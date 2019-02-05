98ab31441264984c16a262dc8cd452107ace0a9d
kalug89 <kaluzny_david@hotmail.com>
2019-02-05 19:43:04 +0100

    End Condiional Stements chapter

---
+++ src/9_Conditional Statements.kt
@@ -0,0 +1,93 @@
+fun main(args: Array<String>) {
+
+    //1
+//    val x = 2
+//    val y = "Słowo"
+//    when (x) {
+//        1 -> println("Mamy jedynkę")
+//        2 -> println("Mamy dwójkę")
+//        3 -> println("Mamy trójkę")
+//        4 -> println("Mamy czwórkę")
+//        else -> println("Nic z powyższych")
+//    }
+
+
+//    val names = listOf("Dawid", "Karolina", "Robercik")
+//    when(names) {
+////        in names -> println("nam to imię")
+////        !in 1..10 -> println("Argument nie jest z zakresu od 1 do 10")
+////        is String -> println(y.length)
+//        else -> println("Coś nie tak")
+//
+
+    //2
+//    val z = 1
+//    val greetting = when(z){
+//        1 -> "jeden"
+//        2 -> "dwa"
+//        3 -> "trzy"
+//        else -> "Nic z powyższych"
+//    }
+//    println(greetting)
+
+    //3
+//    val some = "Helloo"
+//    if (some.length == 5){
+//        println("Słowo ma pięć znaków")
+//    } else if (some.length >= 6){
+//        println("Słowo ma jest równe lub większe 6")
+//    } else {
+//        println("za krótkie")
+//    }
+
+    //4
+//    val dom = 3
+//
+//    val str = if(1 == dom) "Condition met" else "Condition not met"
+//
+//    println(str)
+
+    //
+//    val str = if(2 < dom) {
+//        println("Jest mniejsze od dwóch")
+//    } else if (3 == dom ) {
+//        println("Jest równe 3")
+//    } else {
+//        println("Żadne z podanych")
+//    }
+
+    //5
+
+//    val str = 1
+//    when {
+//        str == 5 -> {
+//            println("woow1")
+//            println("woow2")
+//        }
+//        str == 6 -> println("nakurwiaj na perkusji")
+//        str == 7 -> println("nakurwiaj na bębnie")
+//        else -> println("nic w tym rodzaju")
+//    }
+
+    //6
+//    doSome(Day.poniedzialek)
+//}
+//
+//enum class Day{
+//    poniedzialek,
+//    Wtorek,
+//    Sroda,
+//    Czwartek,
+//    Piatek
+//}
+//
+//fun doSome(day: Day){
+//    when(day){
+//        Day.poniedzialek, Day.Wtorek -> println("Poniedzialek")
+//        Day.Wtorek -> println("Wtorek")
+//        Day.Sroda -> println("Sroda")
+//        Day.Czwartek -> println("Czwartek")
+//        Day.Piatek -> println("Piatek")
+//        else -> println("nic z powyższych")
+//    }
+}
