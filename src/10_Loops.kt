e7991b8aea9b3e9247a8e6f0cb780a5c68d67232
kalug89 <kaluzny_david@hotmail.com>
2019-02-06 20:57:22 +0100

    Make loop exercise

---
+++ src/10_Loops.kt
@@ -0,0 +1,23 @@
+fun main(args: Array<String>) {
+
+
+    //1
+    val list = listOf("Domek", "Korzen", "Obora")
+
+//    for (liter in list){
+//        println(liter)
+//    }
+
+//    list.forEach{
+//        println(it)
+//    }
+
+    //2
+    //    repeat(10){ x ->
+//        println("Jestemy na $x operacji pętli" )
+//        val domek = x
+//    }
+
+
+    //3
+}
