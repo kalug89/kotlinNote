41e3a9b4f61d4cd6e8ed665aa40cddc0252b2924
kalug89 <kaluzny_david@hotmail.com>
2019-02-09 20:27:58 +0100

    End loop exercise and start Ranges

--- src/10_Loops.kt
+++ src/10_Loops.kt
@@ -2,7 +2,7 @@ fun main(args: Array<String>) {
 
 
     //1
-    val list = listOf("Domek", "Korzen", "Obora")
+//    val list = listOf("Domek", "Korzen", "Obora")
 
 //    for (liter in list){
 //        println(liter)
@@ -19,5 +19,34 @@ fun main(args: Array<String>) {
 //    }
 
 
-    //3
+    //4
+
+//    var map = hashMapOf(1 to "domek", 2 to "stromek", 3 to "Drzewko")
+//    for ((value, key) in map){
+//        println("Map$value, $key")
+//    }
+
+    //5
+
+//    var list = listOf(1, 2, 3, 4, 5, 6,7)
+//    var mapList = list.map { "numerek $it" }
+//    println(mapList)
+
+
+    //7
+//    val numbers = listOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 0)
+//    val numberStrings = numbers.filter { it % 2 == 0 }.map {
+//        "Number $it"
+//    }
+
+    //8
+    val numbers = listOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 0)
+    numbers.filter { it % 8 == 0  }.map{
+        println("Numeer $it")
+    }
+
 }
+
+
+
+
